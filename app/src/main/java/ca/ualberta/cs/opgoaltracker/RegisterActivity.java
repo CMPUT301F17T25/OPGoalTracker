8e3095e3c96f62a19237bffce52e93c81de81cb9
Mingwei Li <mingweiarthurli@gmail.com>
2017-11-02 16:56:57 -0600

    add Habit layout

--- app/src/main/java/ca/ualberta/cs/opgoaltracker/RegisterActivity.java
+++ app/src/main/java/ca/ualberta/cs/opgoaltracker/RegisterActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class Register_activity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -87,7 +87,7 @@ public class Register_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_register_activity);
+        setContentView(R.layout.activity_register);
 
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
