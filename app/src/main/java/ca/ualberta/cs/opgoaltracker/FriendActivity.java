1c9ef649ee977c34417ed97548a9d4e5a603dc0e
songdichong <dichong@ualberta.ca>
2017-11-03 20:14:36 -0600

    add following list & follower list. Next section discuss friendlist

---
+++ app/src/main/java/ca/ualberta/cs/opgoaltracker/FriendActivity.java
@@ -0,0 +1,88 @@
+package ca.ualberta.cs.opgoaltracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+//Tab layout. Taken from http://abhiandroid.com/materialdesign/tablayout-example-android-studio.html
+public class FriendActivity extends AppCompatActivity {
+
+    ConstraintLayout constraintLayout;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friendlist);
+
+        // get the reference of FrameLayout and TabLayout
+        constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        tabLayout = (TabLayout) findViewById(R.id.simpleTabLayout);
+        // Create a new Tab named "First"
+        TabLayout.Tab firstTab = tabLayout.newTab();
+        firstTab.setText("following"); // set the Text for the first Tab
+        firstTab.setIcon(R.drawable.ic_launcher); // set an icon for the
+        // first tab
+        tabLayout.addTab(firstTab); // add  the tab at in the TabLayout
+
+        // Create a new Tab named "Second"
+        TabLayout.Tab secondTab = tabLayout.newTab();
+        secondTab.setText("follower"); // set the Text for the second Tab
+        secondTab.setIcon(R.drawable.ic_launcher); // set an icon for the second tab
+        tabLayout.addTab(secondTab); // add  the tab  in the TabLayout
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar1);
+        setSupportActionBar(myToolbar);
+
+        FloatingActionButton addFriend = (FloatingActionButton) findViewById(R.id.add_friend);
+        addFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Click action
+                Intent intent = new Intent(FriendActivity.this, FriendSearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // perform setOnTabSelectedListener event on TabLayout
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+             // get the current selected tab's position and replace the fragment accordingly
+                Fragment fragment = null;
+                switch (tab.getPosition()) {
+                    case 0:
+                        fragment = new FollowingList();
+                        break;
+                    case 1:
+                        fragment = new FollowerList();
+                        break;
+                }
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.constraintLayout, fragment);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                ft.commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
