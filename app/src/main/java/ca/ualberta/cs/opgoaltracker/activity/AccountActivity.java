8ec47f7bb802464eddd5a0f3b84d2f3e7572a0ee
yjia2017 <3120102701@zju.edu.cn>
2017-11-05 22:38:02 -0700

    add transition between account page and main page

--- app/src/main/java/ca/ualberta/cs/opgoaltracker/activity/AccountActivity.java
+++ app/src/main/java/ca/ualberta/cs/opgoaltracker/activity/AccountActivity.java
@@ -1,7 +1,10 @@
 package ca.ualberta.cs.opgoaltracker.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import ca.ualberta.cs.opgoaltracker.R;
 
@@ -11,5 +14,18 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        configureSignUpButton();
+    }
+
+    private void configureSignUpButton(){
+        Button addLogoutButton = (Button) findViewById(R.id.logout);
+        addLogoutButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(AccountActivity.this, MainActivity.class));
+                //Let logout buttion jump to main activity page.
+                finish();
+            }
+        });
     }
 }
