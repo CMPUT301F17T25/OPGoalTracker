c0d264fe60db01fc8d7be143e8d98b2f0a3a0c1a
jajayongjia <yongjiahuang@Yongjias-MacBook-Pro.local>
2017-11-10 22:12:52 -0500

    added copyright

--- app/src/androidTest/java/ca/ualberta/cs/opgoaltracker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/opgoaltracker/ExampleInstrumentedTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2017 Yongjia Huang, Dichong Song, Mingwei Li, Donglin Han, Long Ma,CMPUT301F17T25 CMPUT301, University of Alberta, All Rights Reserved.
+ * You may use distribut, or modify this code under terms and conditions of the ode of Student Behavior at University of Alberta
+ * You may find a copy of the license in this project. Otherwise please contact jajayongjia@gmail.com
+ */
+
 package ca.ualberta.cs.opgoaltracker;
 
 import android.content.Context;
--- app/src/main/java/ca/ualberta/cs/opgoaltracker/activity/MainActivity.java
+++ app/src/main/java/ca/ualberta/cs/opgoaltracker/activity/MainActivity.java
@@ -1,3 +1,10 @@
+
+/*
+ * Copyright 2017 Yongjia Huang, Dichong Song, Mingwei Li, Donglin Han, Long Ma,CMPUT301F17T25 CMPUT301, University of Alberta, All Rights Reserved.
+ * You may use distribut, or modify this code under terms and conditions of the ode of Student Behavior at University of Alberta
+ * You may find a copy of the license in this project. Otherwise please contact jajayongjia@gmail.com
+ */
+
 package ca.ualberta.cs.opgoaltracker.activity;
 
 import android.content.Context;
@@ -34,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText userID;
     private String name;
     private String name2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
