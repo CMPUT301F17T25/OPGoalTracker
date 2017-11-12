517de4c36cbfa1e1dac1e1843787baba497ffc58
jajayongjia <yongjiahuang@Yongjias-MacBook-Pro.local>
2017-11-12 14:34:27 -0500

    added model java doc 1/2

--- app/src/main/java/ca/ualberta/cs/opgoaltracker/models/Admin.java
+++ app/src/main/java/ca/ualberta/cs/opgoaltracker/models/Admin.java
@@ -7,9 +7,13 @@
 package ca.ualberta.cs.opgoaltracker.models;
 
 /**
- * Created by song on 2017/10/23.
+ * This Admin Class is a subclass of User<br>
+ *     This Class allows Admin user to set the restrictions to this app
+ * @author Dichong Song, Yongjia Huang
+ * @version 3.0
+ * @see User
+ * @since 1.0
  */
-
 public class Admin extends User{
 
     public Admin(String id) {
@@ -21,20 +25,52 @@ public class Admin extends User{
     private int reasonLength = 30;
     private int commentLength = 20;
 
+    /**
+     * Basic PicSize getter
+     * @return int PicSize
+     */
     public int getPicSize(){return this.picSize;}
 
+    /**
+     * Basic Title Length getter
+     * @return int titlelenght
+     */
     public int getTitleLength(){return this.titleLength;}
 
+    /**
+     * Basic Reason lenght getter
+     * @return int reason length
+     */
     public int getReasonLength(){return this.reasonLength;}
 
+    /**
+     * Basic CommentLenght getter
+     * @return int commentLenght
+     */
     public int getCommentLength(){return this.commentLength;}
 
+    /**
+     * Basic picSize setter
+     * @param picSize int
+     */
     public void setPicSize(int picSize){this.picSize = picSize;}
 
+    /**
+     * Basic titleLenght Setter
+     * @param titleLength int
+     */
     public void setTitleLength(int titleLength){this.titleLength = titleLength;}
 
+    /**
+     * Basic reasonLength setter
+     * @param reasonLength int
+     */
     public void setReasonLength(int reasonLength){this.reasonLength = reasonLength;}
 
+    /**
+     * Basic commentLength setter
+     * @param commentLength int 
+     */
     public void setCommentLength(int commentLength){this.commentLength = commentLength;}
 
 }
