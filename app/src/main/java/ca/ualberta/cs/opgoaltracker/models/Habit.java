028af9b2ebb24c1ed7713b7b83288e83c30007a6
Mingwei Li <mingweiarthurli@gmail.com>
2017-11-28 16:26:28 -0700

    Add visibility function to Habit list separator.

--- app/src/main/java/ca/ualberta/cs/opgoaltracker/models/Habit.java
+++ app/src/main/java/ca/ualberta/cs/opgoaltracker/models/Habit.java
@@ -11,6 +11,7 @@ import android.os.Parcelable;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import ca.ualberta.cs.opgoaltracker.exception.NoTitleException;
@@ -230,6 +231,24 @@ public class Habit implements Parcelable, Comparable<Habit> {
      */
     public Date getCreateDate() {
         return createDate;
+    }
+
+    public boolean isTodo() {
+        Calendar thisDate = Calendar.getInstance();
+        thisDate.setTime(this.date);
+        Calendar currentDate = Calendar.getInstance();
+
+        if ((thisDate.get(Calendar.YEAR) < currentDate.get(Calendar.YEAR)) ||
+                (thisDate.get(Calendar.YEAR) == currentDate.get(Calendar.YEAR) &&
+                        thisDate.get(Calendar.DAY_OF_YEAR) <= currentDate.get(Calendar.DAY_OF_YEAR))) { // if habit start date is same or before today
+            if (this.period.get(currentDate.get(Calendar.DAY_OF_WEEK) - 1)) { // if today in the week is in the habit period
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     /**
