aed3d44db2c4a19bcf2c848f876961e94db86cf3
Mingwei Li <mingweiarthurli@gmail.com>
2017-12-03 03:21:01 -0700

    Minor UI changes

--- app/src/main/java/ca/ualberta/cs/opgoaltracker/activity/MenuPage.java
+++ app/src/main/java/ca/ualberta/cs/opgoaltracker/activity/MenuPage.java
@@ -202,7 +202,7 @@ public class MenuPage extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_page, menu);
+//        getMenuInflater().inflate(R.menu.menu_page, menu);
         return true;
     }
 
