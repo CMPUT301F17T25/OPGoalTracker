55c0cff6224d76ca7fa2ea6ab10ab236cd9b9de5
Dichong Song <dichong@cs.ualberta.ca>
2017-12-04 16:54:34 -0700

    friendTest

--- app/src/androidTest/java/ca/ualberta/cs/opgoaltracker/activity/FriendTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/opgoaltracker/activity/FriendTest.java
@@ -6,10 +6,19 @@
 
 package ca.ualberta.cs.opgoaltracker.activity;
 
+
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +26,7 @@ import org.junit.runner.RunWith;
 import ca.ualberta.cs.opgoaltracker.R;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
@@ -30,23 +40,72 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 /**
- * Created by song on 2017/12/4.
+ * create by Dichong Song
  */
-
+@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class FriendTest {
+
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void newFriendTest() {
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.userID), isDisplayed()));
-        appCompatEditText.perform(replaceText("111"), closeSoftKeyboard());
-
+    public void friendTest2() {
         ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.signin), withText("Sign In"), isDisplayed()));
+                allOf(withId(R.id.signup), withText("Sign Up"), isDisplayed()));
         appCompatButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.user_id), isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.user_id), isDisplayed()));
+        appCompatEditText2.perform(replaceText("sss"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.user_id), withText("sss"), isDisplayed()));
+        appCompatEditText3.perform(pressImeActionButton());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.register), withText("Register"), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.signup), withText("Sign Up"), isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.user_id), isDisplayed()));
+        appCompatEditText4.perform(click());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.user_id), isDisplayed()));
+        appCompatEditText5.perform(replaceText("ssss"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.user_id), withText("ssss"), isDisplayed()));
+        appCompatEditText6.perform(pressImeActionButton());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.register), withText("Register"), isDisplayed()));
+        appCompatButton4.perform(click());
+
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.userID), isDisplayed()));
+        appCompatEditText7.perform(click());
+
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.userID), isDisplayed()));
+        appCompatEditText8.perform(replaceText("sss"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText9 = onView(
+                allOf(withId(R.id.userID), withText("sss"), isDisplayed()));
+        appCompatEditText9.perform(pressImeActionButton());
+
+        ViewInteraction appCompatButton5 = onView(
+                allOf(withId(R.id.signin), withText("Sign In"), isDisplayed()));
+        appCompatButton5.perform(click());
 
         ViewInteraction appCompatImageButton = onView(
                 allOf(withContentDescription("Open navigation drawer"),
@@ -59,23 +118,179 @@ public class FriendTest {
         appCompatCheckedTextView.perform(click());
 
         ViewInteraction floatingActionButton = onView(
-                allOf(withId(R.id.add_friend), isDisplayed()));
+                allOf(withId(R.id.add_friend),
+                        withParent(withId(R.id.constraintLayout)),
+                        isDisplayed()));
         floatingActionButton.perform(click());
 
-        ViewInteraction appCompatEditText2 = onView(
+        ViewInteraction appCompatEditText10 = onView(
                 allOf(withId(R.id.userID), isDisplayed()));
-        appCompatEditText2.perform(click());
+        appCompatEditText10.perform(click());
 
-        ViewInteraction appCompatEditText3 = onView(
+        ViewInteraction appCompatEditText11 = onView(
                 allOf(withId(R.id.userID), isDisplayed()));
-        appCompatEditText3.perform(replaceText("222"), closeSoftKeyboard());
+        appCompatEditText11.perform(replaceText("ssss"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText4 = onView(
-                allOf(withId(R.id.userID), withText("222"), isDisplayed()));
-        appCompatEditText4.perform(pressImeActionButton());
+        ViewInteraction appCompatEditText12 = onView(
+                allOf(withId(R.id.userID), withText("ssss"), isDisplayed()));
+        appCompatEditText12.perform(pressImeActionButton());
 
-        ViewInteraction appCompatButton2 = onView(
+        ViewInteraction appCompatButton6 = onView(
                 allOf(withId(R.id.search), withText("search"), isDisplayed()));
-        appCompatButton2.perform(click());
+        appCompatButton6.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.name), withText("ssss"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("ssss")));
+
+        ViewInteraction appCompatButton7 = onView(
+                allOf(withId(R.id.follow), withText("follow"), isDisplayed()));
+        appCompatButton7.perform(click());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction appCompatCheckedTextView2 = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("setting"), isDisplayed()));
+        appCompatCheckedTextView2.perform(click());
+
+        ViewInteraction appCompatButton8 = onView(
+                allOf(withId(R.id.logout), withText("LOG OUT"), isDisplayed()));
+        appCompatButton8.perform(click());
+
+        ViewInteraction appCompatEditText13 = onView(
+                allOf(withId(R.id.userID), isDisplayed()));
+        appCompatEditText13.perform(click());
+
+        ViewInteraction appCompatEditText14 = onView(
+                allOf(withId(R.id.userID), isDisplayed()));
+        appCompatEditText14.perform(replaceText("ssss"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction appCompatButton9 = onView(
+                allOf(withId(R.id.signin), withText("Sign In"), isDisplayed()));
+        appCompatButton9.perform(click());
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
+
+        ViewInteraction appCompatCheckedTextView3 = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Friends"), isDisplayed()));
+        appCompatCheckedTextView3.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withText("request"), isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.userName), withText("sss"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView2.check(matches(withText("sss")));
+
+        ViewInteraction appCompatButton10 = onView(
+                allOf(withId(R.id.accept), withText("accept"), isDisplayed()));
+        appCompatButton10.perform(click());
+
+        ViewInteraction appCompatTextView2 = onView(
+                allOf(withText("follower"), isDisplayed()));
+        appCompatTextView2.perform(click());
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.userName), withText("sss"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView3.check(matches(withText("sss")));
+
+        ViewInteraction appCompatImageButton4 = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton4.perform(click());
+
+        ViewInteraction appCompatCheckedTextView4 = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("setting"), isDisplayed()));
+        appCompatCheckedTextView4.perform(click());
+
+        ViewInteraction appCompatButton11 = onView(
+                allOf(withId(R.id.logout), withText("LOG OUT"), isDisplayed()));
+        appCompatButton11.perform(click());
+
+        ViewInteraction appCompatEditText15 = onView(
+                allOf(withId(R.id.userID), isDisplayed()));
+        appCompatEditText15.perform(click());
+
+        ViewInteraction appCompatEditText16 = onView(
+                allOf(withId(R.id.userID), isDisplayed()));
+        appCompatEditText16.perform(replaceText("sss"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText17 = onView(
+                allOf(withId(R.id.userID), withText("sss"), isDisplayed()));
+        appCompatEditText17.perform(pressImeActionButton());
+
+        ViewInteraction appCompatButton12 = onView(
+                allOf(withId(R.id.signin), withText("Sign In"), isDisplayed()));
+        appCompatButton12.perform(click());
+
+        ViewInteraction appCompatImageButton5 = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton5.perform(click());
+
+        ViewInteraction appCompatCheckedTextView5 = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Friends"), isDisplayed()));
+        appCompatCheckedTextView5.perform(click());
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.userName), withText("ssss"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView4.check(matches(withText("ssss")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
